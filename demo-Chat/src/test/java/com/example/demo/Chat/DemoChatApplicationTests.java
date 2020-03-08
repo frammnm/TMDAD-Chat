239c8529ec3938f0d3b84cda5811ee4ee8097be3
francisco martinez <frammnm@gmail.com>
2020-03-08 20:09:50 +0100

    Started demo project

---
+++ demo-Chat/src/test/java/com/example/demo/Chat/DemoChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.Chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
