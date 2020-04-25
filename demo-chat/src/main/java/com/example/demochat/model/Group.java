1e50c0b2532f2a777ce3e252e8f0748db676e9c3
francisco martinez <frammnm@gmail.com>
2020-04-25 19:03:17 +0200

    Added clases for group, and user, their relevant relationships and the modifications needed in the database

---
+++ demo-chat/src/main/java/com/example/demochat/model/Group.java
@@ -0,0 +1,87 @@
+package com.example.demochat.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "groups")
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "owner")
+    private String owner;
+
+    @Column(name = "url")
+    private String url;
+
+    @Column(name = "messages")
+    @OneToMany(mappedBy = "group")
+    private List<Message> messages;
+
+    @ManyToMany(mappedBy = "groups")
+    private List<User> members;
+
+    public Group(String name, String owner) {
+        this.name = name;
+        this.owner = owner;
+    }
+
+    public Group() {
+        super();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public List<User> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<User> members) {
+        this.members = members;
+    }
+
+}
