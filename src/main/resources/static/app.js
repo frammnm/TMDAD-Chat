57b760dcea05d0364429b5a9fa5a76b7f3b427a1
francisco martinez <frammnm@gmail.com>
2020-05-04 17:57:58 +0200

    Changed project structure

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,274 @@
+var stompClient = null;
+var apiURL = "/api/v1/";
+var user = null;
+var defaultTimeout = 120000;
+
+//START FUNCTIONS
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#messages").html("");
+}
+
+function connect() {
+    let socket = new SockJS('/stomp');
+    let username = $("#username").val();
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+
+        //Get user
+        getUser(username);
+
+        //Subscribe to personal messages
+        stompClient.subscribe('/queue/'+username, function (message) {
+                console.log(message);
+                showMessage(JSON.parse(message.body));
+        });
+
+        //Subscribe to system alerts
+        stompClient.subscribe('/topic/all', function (message) {
+            console.log(message);
+            showMessage(JSON.parse(message.body));
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendMessage() {
+    let sendType = $('#sendMessageModal').data("sending-type");
+
+    let message = {
+        from: $("#username").val(),
+        body: $("#message-text").val(),
+        to: $("#recipient-name").val(),
+        timestamp: Date.now()
+    }
+
+    switch(sendType) {
+        case 'file':
+            uploadFile(message);
+            break;
+        case 'textAll':
+            message.to = 'all';
+            //Send text message
+            stompClient.send("/app/messageAll", {}, JSON.stringify(message));
+            break;
+        case "textGroup":
+            //case: text
+            //Send text message
+            stompClient.send("/app/groupMessage", {}, JSON.stringify(message));
+            break;
+        default:
+            //case: text
+            //Send text message
+            stompClient.send("/app/message", {}, JSON.stringify(message));
+    }
+}
+
+function uploadFile(message) {
+    let formData = new FormData();
+    formData.append('message',
+        new Blob([JSON.stringify(message)],
+            {
+                type: "application/json"
+            }
+        )
+    );
+    // Attach file
+    formData.append('file', $('#uploadFile')[0].files[0]);
+
+    $.ajax({
+        url: '/files/upload',
+        data: formData,
+        type: 'POST',
+        //cache: false,
+        //timeout: 600000,
+        contentType: false, // NEEDED, DON'T OMIT THIS (requires jQuery 1.6+)
+        processData: false, // NEEDED, DON'T OMIT THIS
+        success: function(msg) {
+            console.log(msg);
+        },
+        error: function(err) {
+            console.log(err.responseJSON);
+        }
+    });
+}
+
+function showMessage(message) {
+    let msgTemplate = "<tr>";
+    let date = new Date(parseInt(message.timestamp));
+    msgTemplate += "<td>"+ date.getHours() + ":" + addZero(date.getMinutes()) +"</td>";
+    msgTemplate += "<td>"+ message.from +"</td>";
+    msgTemplate += "<td>"+ message.body +"</td>";
+    msgTemplate += "</tr>";
+    $("#messages").append(msgTemplate);
+}
+
+function getUser(username){
+    $.ajax({
+        url: apiURL+"/users/byUsername/"+username,
+        type: "GET",
+        contentType: 'application/json; charset=utf-8',
+        success: function(resultData) {
+            user = resultData;
+            console.log(user);
+            subscribeToGroups();
+        },
+        error : function(jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown);
+        },
+        timeout: defaultTimeout,
+    });
+}
+
+function subscribeToGroups() {
+    if (user == null) {
+        return;
+    }
+
+    user.groups.forEach(function (group) {
+        //Subscribe to personal messages
+        stompClient.subscribe('/topic/' + group.name, function (message) {
+            console.log(message);
+            showMessage(JSON.parse(message.body));
+        });
+    });
+
+    user.ownedGroups.forEach(function (group) {
+        //Subscribe to personal messages
+        stompClient.subscribe('/topic/' + group.name, function (message) {
+            console.log(message);
+            showMessage(JSON.parse(message.body));
+        });
+    });
+}
+
+function getOldMessages(){
+    $.ajax({
+        url: apiURL+"/messages/",
+        type: "GET",
+        contentType: 'application/json; charset=utf-8',
+        success: function(resultData) {
+            //console.log(resultData);
+            let username = $("#username").val();
+            resultData.forEach(function(message) {
+                if (message.to == username){
+                    showMessage(message);
+                }
+            });
+        },
+        error : function(jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown);
+        },
+        timeout: defaultTimeout,
+    });
+}
+
+function createGroup(){
+
+    let group =     {
+        id: 17,
+        name: "test1",
+        owner: {
+            id: 1,
+            username: "francisco",
+            password: "admin1",
+            role: "admin",
+            groups: [],
+            ownedGroups: [
+                17
+            ]
+        },
+        url: "/groups/test11291253989",
+        messages: [],
+        members: []
+    }
+
+    $.ajax({
+        url: apiURL+"/groups/create",
+        type: "POST",
+        contentType: 'application/json; charset=utf-8',
+        data: group,
+        success: function(resultData) {
+            console.log(resultData);
+        },
+        error : function(jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown);
+        },
+        timeout: defaultTimeout,
+    });
+}
+
+$(function () {
+    setConnected(false);
+
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect();});
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendMessage(); $('#sendMessageModal').modal('hide')});
+
+    $('#sendMessageModal').on('show.bs.modal', function (event) {
+        let button = $(event.relatedTarget); // Button that triggered the modal
+        let sendType = button.data('sending');
+        let modal = $(this);
+
+        modal.data("sending-type", sendType);
+
+        switch(sendType) {
+            case "file":
+                $("#fileFormField").show();
+                $("#textRecipientField").show();
+                $("#textFormField").hide();
+                break;
+            case "textAll":
+                $("#textFormField").show();
+                $("#textRecipientField").hide();
+                $("#fileFormField").hide();
+
+                modal.find('.modal-body textarea').val('');
+                break;
+            case "textGroup":
+                $("#textFormField").show();
+                $("#textRecipientField").show();
+                $("#fileFormField").hide();
+
+                modal.find('.modal-body textarea').val('');
+                break;
+            default:
+                //case: text
+                $("#textFormField").show();
+                $("#textRecipientField").show();
+                $("#fileFormField").hide();
+
+                modal.find('.modal-body textarea').val('');
+        }
+
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    })
+});
