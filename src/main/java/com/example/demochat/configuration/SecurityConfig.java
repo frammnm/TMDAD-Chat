0d464172f545af4fded394ad4493c7e11a95c4cd
802185 <61416565+802185@users.noreply.github.com>
2020-05-20 22:49:28 +0200

    incluyendo permiso a los resources del proyecto

--- src/main/java/com/example/demochat/configuration/SecurityConfig.java
+++ src/main/java/com/example/demochat/configuration/SecurityConfig.java
@@ -48,7 +48,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/register",
 
             // Project resources
-//            "/**", //Validar todo pq no sirve
+            "/css/**",
+            "/js/**",
+            "/favicon.ico",
+            "/appLogo.png",
 
             // -- swagger ui
             "/swagger-resources/**",
