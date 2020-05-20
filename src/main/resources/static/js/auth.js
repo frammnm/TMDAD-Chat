344bb9121c24244834fd1fc64367c5fa66fc1bab
802185 <61416565+802185@users.noreply.github.com>
2020-05-20 21:21:22 +0200

    agregando registro del app

--- src/main/resources/static/js/auth.js
+++ src/main/resources/static/js/auth.js
@@ -39,12 +39,45 @@ function login(){
     });
 }
 
+function register(){
+    if (!validate()){
+        $('#systemAlert .alert-heading').text('Incorrect Login');
+        $('#systemAlert p').text('Username and password are required');
+        $('#systemAlert').fadeIn();
+        return;
+    }
+
+    //Create User
+    let user = {
+        username: $('#username').val(),
+        password: $('#password').val()
+    }
+
+    $.ajax({
+        url: apiURL+'/users/signup',
+        data: JSON.stringify(user),
+        type: 'POST',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function(res) {
+            console.log(res);
+            login();
+        },
+        error: function(err) {
+            console.log(err.responseJSON);
+            $('#systemAlert .alert-heading').text('Incorrect Login');
+            $('#systemAlert p').text('Username or password incorrect');
+            $('#systemAlert').fadeIn();
+        }
+    });
+}
 
 $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault();
     });
     $('body').on('click', '#send-button', function() { login();});
+    $('body').on('click', '#register-button', function() { register();});
 
     //This is to be able to hide alerts
     $("[data-hide]").on("click", function(){
